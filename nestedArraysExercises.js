1b5fa2f30f71e692e020b03d487e105794a73be9
PJ Mantoss <46089979+PJMantoss@users.noreply.github.com>
2020-02-19 10:47:25 +0100

    update nestedArraysExercises.js

--- nestedArraysExercises.js
+++ nestedArraysExercises.js
@@ -14,4 +14,19 @@ function printEvens(){
 };
 
 
-printEvens();
+printEvens(); // 2,4,6,8,10,12
+
+//Ex 2: Given the following array, write a function called sumTotal returns the sum of all numbers in the array
+var nestedArr = [[1, 2], [3, 4], [5, 6]];
+
+function sumTotal(){
+    let totalSum = 0;
+    for (let i=0; i < nestedArr.length; i++){
+        for (let j=0; j < nestedArr[i].length; j++){
+           totalSum += nestedArr[i][j];
+        }
+    }
+    console.log(totalSum);
+}
+
+sumTotal(); //21
