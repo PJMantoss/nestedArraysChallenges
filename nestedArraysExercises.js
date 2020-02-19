8ee58f38ab1a79aeb5e817dc9b286e0c0b3c0936
PJ Mantoss <46089979+PJMantoss@users.noreply.github.com>
2020-02-19 10:18:55 +0100

    update nestedArraysExercises.js

--- nestedArraysExercises.js
+++ nestedArraysExercises.js
@@ -1,1 +1,17 @@
+//Exercise 1: Given the following array, write a function called printEvens that console.logs all of the even numbers
 
+var nestedArr = [[1, 2, 3], [4, 5, 6], [7, 8], [9, 10, 11, 12]];
+
+function printEvens(){
+   let newArr = nestedArr;
+    for (let i=0; i<newArr.length; i++){
+        for (let j=0; j < newArr[i].length; j++){
+            if (newArr[i][j] % 2 === 0){
+                console.log(newArr[i][j]);        
+            }
+        }
+    }
+};
+
+
+printEvens();
